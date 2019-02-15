899b0b3ccb6bda084b27a9b217135dcd17ba9b14
KouT127 <kou26haru@gmail.com>
2019-02-15 22:08:35 +0900

    Add StickyHeader in TimeTableView

--- conf_clone_app/lib/ui/pages/time_table.dart
+++ conf_clone_app/lib/ui/pages/time_table.dart
@@ -1,4 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:sticky_headers/sticky_headers.dart';
+
+class TimeTableView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Flex(
+      direction: Axis.vertical,
+      children: <Widget>[
+        SizedBox(
+          height: 25.0,
+          width: double.infinity,
+          child: Flex(
+            direction: Axis.horizontal,
+            children: <Widget>[
+              Text(
+                '2019.2.14',
+                style: TextStyle(fontSize: 22.0, color: Colors.grey),
+              )
+            ],
+          ),
+        ),
+        Expanded(child: TimeTableList())
+      ],
+    );
+  }
+}
 
 class TimeTableList extends StatefulWidget {
   @override
@@ -8,9 +34,31 @@ class TimeTableList extends StatefulWidget {
 class _TimeTableListState extends State<TimeTableList> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    double sectionWidth = size.width / 4.0;
+
     return ListView.builder(
       itemBuilder: (context, index) {
-        return TimeTableTile(PresentationList[index]);
+        return StickyHeader(
+            header: Container(
+              height: 15,
+              child: SizedBox(
+                width: sectionWidth,
+                height: double.infinity,
+                child: Text(
+                  PresentationList[index].getStringTime(),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 19.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            content: Container(
+              margin: EdgeInsets.fromLTRB(sectionWidth, 10, 10, 10),
+              child: TimeTableTile(PresentationList[index]),
+            ));
       },
       itemCount: PresentationList.length,
     );
@@ -28,33 +76,24 @@ class TimeTableTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.fromLTRB(100, 10, 10, 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              presentation.title,
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                  fontSize: 15.0,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
-            Text(
-                presentation.name,
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    fontSize: 15.0,
-                    color: Colors.black,
-                )
-            ),
-          Wrap(spacing: 5.0,
-            children: _buildTags(presentation.tags)
-          )
-        ],
-      )
-    );
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          presentation.title,
+          textAlign: TextAlign.left,
+          style: TextStyle(
+              fontSize: 15.0, color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        Text(presentation.name,
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 15.0,
+              color: Colors.black,
+            )),
+        Wrap(spacing: 5.0, children: _buildTags(presentation.tags))
+      ],
+    ));
   }
 }
 
@@ -71,16 +110,114 @@ List<Widget> _buildTags(List<String> tags) {
 }
 
 const PresentationList = [
-  Presentation(title: 'マルチモジュールなプロジェクトでテストはどう変わる？', name: 'Name', content: 'content', tags: [
-    '初心者',
-    'Flutter',
-    'Web',
-  ]),
-  Presentation(title: 'マルチモジュールなプロジェクトでテストはどう変わる？', name: 'Name', content: 'content', tags: [
-    '初心者',
-    'Flutter',
-    'Web',
-  ]),
+  Presentation(
+      title: 'マルチモジュールなプロジェクトでテストはどう変わる？',
+      name: 'Name',
+      content: '19:20',
+      tags: [
+        '初心者',
+        'Flutter',
+        'Web',
+      ]),
+  Presentation(
+      title: 'マルチモジュールなプロジェクトでテストはどう変わる？',
+      name: 'Name',
+      content: '19:30^',
+      tags: [
+        '初心者',
+        'Flutter',
+        'Web',
+      ]),
+  Presentation(
+      title: 'マルチモジュールなプロジェクトでテストはどう変わる？',
+      name: 'Name',
+      content: 'content',
+      tags: [
+        '初心者',
+        'Flutter',
+        'Web',
+      ]),
+  Presentation(
+      title: 'マルチモジュールなプロジェクトでテストはどう変わる？',
+      name: 'Name',
+      content: 'content',
+      tags: [
+        '初心者',
+        'Flutter',
+        'Web',
+      ]),
+  Presentation(
+      title: 'マルチモジュールなプロジェクトでテストはどう変わる？',
+      name: 'Name',
+      content: 'content',
+      tags: [
+        '初心者',
+        'Flutter',
+        'Web',
+      ]),
+  Presentation(
+      title: 'マルチモジュールなプロジェクトでテストはどう変わる？',
+      name: 'Name',
+      content: 'content',
+      tags: [
+        '初心者',
+        'Flutter',
+        'Web',
+      ]),
+  Presentation(
+      title: 'マルチモジュールなプロジェクトでテストはどう変わる？',
+      name: 'Name',
+      content: '19:20',
+      tags: [
+        '初心者',
+        'Flutter',
+        'Web',
+      ]),
+  Presentation(
+      title: 'マルチモジュールなプロジェクトでテストはどう変わる？',
+      name: 'Name',
+      content: '19:30^',
+      tags: [
+        '初心者',
+        'Flutter',
+        'Web',
+      ]),
+  Presentation(
+      title: 'マルチモジュールなプロジェクトでテストはどう変わる？',
+      name: 'Name',
+      content: 'content',
+      tags: [
+        '初心者',
+        'Flutter',
+        'Web',
+      ]),
+  Presentation(
+      title: 'マルチモジュールなプロジェクトでテストはどう変わる？',
+      name: 'Name',
+      content: 'content',
+      tags: [
+        '初心者',
+        'Flutter',
+        'Web',
+      ]),
+  Presentation(
+      title: 'マルチモジュールなプロジェクトでテストはどう変わる？',
+      name: 'Name',
+      content: 'content',
+      tags: [
+        '初心者',
+        'Flutter',
+        'Web',
+      ]),
+  Presentation(
+      title: 'マルチモジュールなプロジェクトでテストはどう変わる？',
+      name: 'Name',
+      content: 'content',
+      tags: [
+        '初心者',
+        'Flutter',
+        'Web',
+      ]),
 ];
 
 class Presentation {
@@ -90,16 +227,15 @@ class Presentation {
   final List<String> tags;
   final DateTime time;
 
-  const Presentation({
-    this.title,
-    this.name,
-    this.content,
-    this.tags,
-    this.time
-  });
+  const Presentation(
+      {this.title, this.name, this.content, this.tags, this.time});
 
   @override
   String toString() {
     return 'Presentation { title: $title, content: $content, tags: $tags)';
+  }
+
+  String getStringTime() {
+    return '10:00';
   }
 }
